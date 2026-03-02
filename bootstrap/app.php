6149a08b06173b2e4aa482f1b6a5b1f9562bb3fe
Emillia Cantella <emilliacantella@Emillias-MacBook-Air.local>
2026-03-02 18:44:25 +0800

    upload

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
+    ->useStoragePath('/tmp') // <- INI KUNCI FIX VERCEL
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -19,7 +20,6 @@ $app = Application::configure(basePath: dirname(__DIR__))
 
         $middleware->validateCsrfTokens(except: [
             'broadcasting/auth',
-            'livewire/upload-file'
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
@@ -27,22 +27,51 @@ $app = Application::configure(basePath: dirname(__DIR__))
     })
     ->create();
 
-// Override storage path ke /tmp (writable di Vercel)
-$app->useStoragePath('/tmp');
+//     <?php
 
-// Pastikan folder yang dibutuhkan Laravel ada
-$paths = [
-    '/tmp/framework',
-    '/tmp/framework/cache',
-    '/tmp/framework/views',
-    '/tmp/framework/sessions',
-    '/tmp/logs',
-];
+// use Illuminate\Foundation\Application;
+// use Illuminate\Foundation\Configuration\Exceptions;
+// use Illuminate\Foundation\Configuration\Middleware;
 
-foreach ($paths as $path) {
-    if (!is_dir($path)) {
-        mkdir($path, 0777, true);
-    }
-}
+// $app = Application::configure(basePath: dirname(__DIR__))
+//     ->withRouting(
+//         web: __DIR__.'/../routes/web.php',
+//         commands: __DIR__.'/../routes/console.php',
+//         channels: __DIR__.'/../routes/channels.php',
+//         health: '/up',
+//         api: __DIR__.'/../routes/api.php',
+//     )
+//     ->withMiddleware(function (Middleware $middleware): void {
+//         $middleware->alias([
+//             'verify.bot' => \App\Http\Middleware\VerifyBotToken::class,
+//         ]);
 
-return $app;
+//         $middleware->validateCsrfTokens(except: [
+//             'broadcasting/auth',
+//             'livewire/upload-file'
+//         ]);
+//     })
+//     ->withExceptions(function (Exceptions $exceptions): void {
+//         //
+//     })
+//     ->create();
+
+// // Override storage path ke /tmp (writable di Vercel)
+// $app->useStoragePath('/tmp');
+
+// // Pastikan folder yang dibutuhkan Laravel ada
+// $paths = [
+//     '/tmp/framework',
+//     '/tmp/framework/cache',
+//     '/tmp/framework/views',
+//     '/tmp/framework/sessions',
+//     '/tmp/logs',
+// ];
+
+// foreach ($paths as $path) {
+//     if (!is_dir($path)) {
+//         mkdir($path, 0777, true);
+//     }
+// }
+
+// return $app;
